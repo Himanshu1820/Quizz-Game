54b921728691d875eaab28cd3026722751d2535c
Himanshu1820 <himanshu100ni.sikar@gmail.com>
2024-09-23 15:51:02 +0530

    quizz game2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Context from "./context/context.jsx";
+import Context from "./context/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

8c57a46dfc0a93e7c80fe9043d96321a02bf83f3
Himanshu1820 <himanshu100ni.sikar@gmail.com>
2024-09-23 16:41:21 +0530

    Fix: 404_Not Found Error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Define an appropriate fallback for SPA
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true, // For client-side routing support
+  },
+});
